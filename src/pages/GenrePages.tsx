1c8392fe5e7115f623093ac3a489508b404a8d51
Vsanotskyi <vsanotskyi@gmail.com>
2024-03-01 03:47:09 +0200

    finish6

--- src/pages/GenrePages.tsx
+++ src/pages/GenrePages.tsx
@@ -3,15 +3,14 @@ import { useLocation, useSearchParams } from 'react-router-dom';
 
 import { api } from '../services';
 import { IMovie } from '../interfaces';
-import { List, MovieItem, PaginationContainer, Error } from '../components';
-import { ResetPageContext, LoadingContext, ErrorContext } from '../hoc';
+import { List, MovieItem, PaginationContainer } from '../components';
+import { ResetPageContext, LoadingContext } from '../hoc';
 
 const GenrePages = () => {
   const { pathname } = useLocation();
   const [paramsPage, setParamsPage] = useSearchParams({ page: '1' });
   const resPage = useContext(ResetPageContext);
   const loading = useContext(LoadingContext);
-  const error = useContext(ErrorContext);
   const [movies, setMovies] = useState<IMovie[]>([]);
   // @ts-ignore
   const [page, setPage] = useState(paramsPage.get('page') ? +(paramsPage.get('page')) : 1);
@@ -21,21 +20,18 @@ const GenrePages = () => {
 
   const getMovies = async (id: string, page: number) => {
     loading?.setIsLoading(true);
-    error?.setError(null);
     try {
       const { data } = await api.getByGenre(id, page);
       setMovies(data.results);
       setTotalPage(data['total_pages']);
     } catch (err) {
-      const e = err as Error;
-
-      error?.setError(e.message);
+      console.error(err);
     } finally {
       loading?.setIsLoading(false);
     }
   };
 
-  const handleChange = (event: ChangeEvent<unknown>, value: number) => {
+  const handleChange = (_: ChangeEvent<unknown>, value: number) => {
     setPage(value);
     resPage?.setIsReset(false);
   };
@@ -50,7 +46,7 @@ const GenrePages = () => {
     setParamsPage({ page: page.toString() });
 
     getMovies(genreId, page);
-  }, [genreId, page, setParamsPage, error?.error, loading?.isLoading]);
+  }, [genreId, page]);
 
   return (
     <div>
@@ -67,8 +63,6 @@ const GenrePages = () => {
                              page={page}
                              handleChange={handleChange}
         />)}
-
-      {error?.error && <Error title={error.error} />}
     </div>
   );
 };
