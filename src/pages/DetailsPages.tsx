2cdcfcb499ade0b6ece0038f50a59646d5293fdd
Vsanotskyi <vsanotskyi@gmail.com>
2024-03-01 03:59:04 +0200

    finish9

--- src/pages/DetailsPages.tsx
+++ src/pages/DetailsPages.tsx
@@ -1,32 +1,23 @@
-import { useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { api } from '../services';
 import { IDetails } from '../interfaces';
-import { DetailsItem, Error } from '../components';
-import { LoadingContext, ErrorContext } from '../hoc';
+import { DetailsItem } from '../components';
 
 const DetailsPages = () => {
   window.scrollTo(0, 0);
 
-  const loading = useContext(LoadingContext);
-  const error = useContext(ErrorContext);
   const [movieDetails, setMovieDetails] = useState<IDetails | null>(null);
   const { pathname } = useLocation();
   const movieId = pathname.split('/')[pathname.split('/').length - 1];
 
   const getDetails = async (id: string) => {
-    loading?.setIsLoading(true);
-    error?.setError(null);
     try {
       const { data } = await api.getDetailsMovie(id);
       setMovieDetails(data);
     } catch (err) {
-      const e = err as Error;
-
-      error?.setError(e.message);
-    } finally {
-      loading?.setIsLoading(false);
+      console.error(err);
     }
   };
 
@@ -37,7 +28,6 @@ const DetailsPages = () => {
   return (
     <div>
       {movieDetails && <DetailsItem item={movieDetails} />}
-      {error?.error && <Error title={error.error} />}
     </div>
   );
 };
